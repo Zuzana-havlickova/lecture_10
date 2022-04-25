d81025e24673e508f5ffd774876d4b57b07dc614
Zuzana Havlickova <zuzanica@email.cz>
2022-04-25 08:53:46 +0200

    prvni com

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -10,10 +11,21 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for key, value in row.items():
+                if key not in data:
+                    data[key] = []
+                data[key].append(int(value))
+    return data
+
 
 
 def main():
-    pass
+    data = read_data("numbers.csv")
+    print(data)
 
 
 if __name__ == '__main__':
